99cd2424be92359850a1c2b27a83137ee51b10d9
Jameel Mahagna <vip84@outlook.com>
2023-07-26 23:30:22 +0300

    input with split

--- lesson_01_basics/01_input_function/03_input_numbers_split.py
+++ lesson_01_basics/01_input_function/03_input_numbers_split.py
@@ -1,2 +1,4 @@
-score1, score2 = input("Enter your scores: ").split()
-print(score1, score2)
+scores, scores2 = input("what are your scores: ").split()
+scores = int(scores)
+scores2 = int(scores2)
+print(scores, type(scores), scores2, type(scores2))
