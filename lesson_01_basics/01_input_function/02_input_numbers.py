feb7a4cdce800ce9fb8b7a3e391d263a88447c6e
Jameel Mahagna <vip84@outlook.com>
2023-07-26 23:17:07 +0300

    input func

--- lesson_01_basics/01_input_function/02_input_numbers.py
+++ lesson_01_basics/01_input_function/02_input_numbers.py
@@ -1,4 +1,4 @@
-number_1 = int(input(" Please enter your age: "))
-number_2 = int (input("Please enter your name: "))
-print(type(number_2))
-print(f"{number_1}+{number_2} = {number_1}+{number_2}")
+number_1 = input(" Please enter your 1st num: ")
+number_2 = input("Please enter your 2nd num: ")
+print(type (number_2))
+print(f"{number_1} + {number_2} = {number_1 + number_2 }")
