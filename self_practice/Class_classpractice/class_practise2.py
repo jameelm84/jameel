2a11f8332e0337818b3538d1b1c0226f9623a763
Jameel Mahagna <vip84@outlook.com>
2023-12-23 01:00:01 +0200

    classescar

--- self_practice/Class_classpractice/class_practise2.py
+++ self_practice/Class_classpractice/class_practise2.py
@@ -1,29 +1,22 @@
 class MyCar:
-    def __init__(self, brand,year):
+    def __init__(self, brand,year): # constructor
         self.brand = brand
         self.year = year
-    def __init__(self, brand,year):
-        self.brand = brand
-        self.year = year
+    def my_car(self):
+        print(f"this is my car {self.brand} {self.year}")
 
-    # year = 2023
-    # brand = "Tesla"
-
-    def my_drive(self):
-        print("Tesla - 2023 this is my driving")
-    def my_drive2(self):
-        print("BMW - 2024 this is my driving")
     def go_speed(self, max_speed: int):
-        print("up to speed", max_speed)
+        print(f"up to speed {self.brand} {max_speed}")
 
 
 syarte_tesla = MyCar("tesla",2023)
+syarte_tesla.my_car()
 syarte_bmw = MyCar("BMW", 2024)
-print(syarte_tesla.brand,syarte_tesla.year)
-print(syarte_bmw.brand,syarte_bmw.year)
-# print(type(syarte))
-# print(syarte.year)
-# print(syarte.brand)
-syarte_tesla.my_drive()
-syarte_bmw.my_drive2()
-syarte_tesla.go_speed(100)
+syarte_bmw.my_car()
+syarte_tesla.go_speed(300)
+syarte_bmw.go_speed(200)
+# print(syarte_tesla.brand,syarte_tesla.year)
+# print(syarte_bmw.brand,syarte_bmw.year)
+# syarte_tesla.my_car()
+# syarte_bmw.my_car()
+# syarte_tesla.go_speed(100)
