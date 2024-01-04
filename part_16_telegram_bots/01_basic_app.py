8ad12c5e621be276e4a9fa6040f653b055a639ba
Jameel Mahagna <vip84@outlook.com>
2024-01-04 19:53:08 +0200

    byjameel

---
+++ part_16_telegram_bots/01_basic_app.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+
+BOT_TOKEN = "6842779414:AAGLwf8lA5sOjdOsGousW58gtRZUuLuhLUA"
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Promise -> pending / fulfilled / rejected
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+
+# print(f"{__name__ = }")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
--- self_practice/flet/flet_example.py
+++ self_practice/flet/flet_example.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    ft.ElevatedButton(text="Click Me")cut
+    ft.ElevatedButton(text="Click Me")
     ft.Row(controls=[10])
     text_element = ft.Text(value="Hi all", color="red", size=20)
     page.add(text_element)
